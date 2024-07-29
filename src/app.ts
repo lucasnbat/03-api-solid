8d673c91c5f60661e089b5281b5dff6457fcd8e8
Lucas Nunes Batista <lucasnunesb6@gmail.com>
2024-07-28 22:56:21 -0400

    adiciona primeira rota: POST /users

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,30 @@
 import fastify from 'fastify';
 import { PrismaClient } from '@prisma/client'
+import { z } from 'zod';
+import { prisma } from './lib/prisma';
 
 export const app = fastify();
 
-const prisma = new PrismaClient();
+app.post('/users', async (request, reply) => {
+    const registerBodySchema = z.object({
+        name: z.string(),
+        email: z.string().email(),
+        password: z.string().min(6),
+    })
 
-prisma.user.create({
-    data: {
-        name: 'Lucas Batista',
-        email: 'lucas@teste.com'
-    }
+    const {
+        name,
+        email,
+        password
+    } = registerBodySchema.parse(request.body)
+
+    await prisma.user.create({
+        data: {
+            name,
+            email,
+            passord_hash: password,
+        }
+    })
+
+    return reply.status(201).send();
 })
