ccb84da87aa4a398fe4435ff91d90c87c2de2230
Lucas Nunes Batista <lucasnunesb6@gmail.com>
2024-07-30 14:25:25 -0400

    adiciona tratamento de erro para email duplicado; demonstra como mostrar a mensagem de erro;

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,5 +1,6 @@
 import { UsersRepository } from '@/repositories/users-repository'
 import { hash } from 'bcryptjs'
+import { UserAlreadyExistsError } from './errors/user-already-exists-error'
 
 /**
  * Sobre Dependency Inversion (s.o.l.i.D)
@@ -40,7 +41,7 @@ export class RegisterUseCase {
     const userWithSameEmail = await this.usersRepository.findByEmail(email)
 
     if (userWithSameEmail) {
-      throw new Error('E-mail already exists')
+      throw new UserAlreadyExistsError()
     }
 
     await this.usersRepository.create({
