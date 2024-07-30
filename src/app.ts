6de56e0d8369c8a5b13086f439186270b2a67713
Lucas Nunes Batista <lucasnunesb6@gmail.com>
2024-07-30 14:38:57 -0400

    adiciona handler de erros globais: formatação e captura de logs para dev environment

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import fastify from 'fastify'
 import { appRoutes } from './http/routes'
+import { ZodError } from 'zod'
+import { env } from './env'
 
 export const app = fastify()
 
@@ -9,3 +11,20 @@ export const app = fastify()
  * sim, o routes.ts com appRouter também é plugin do fastify)
  */
 app.register(appRoutes)
+
+/* Usa _ quando não vai usar o parametro da função (no caso, request) */
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    return reply
+      .status(400)
+      .send({ message: 'Validation Error', issues: error.format() })
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.error(error)
+  } else {
+    // here we sould log to an external tool like datalogs
+  }
+
+  return reply.status(500).send({ message: 'Internal Server Error' })
+})
