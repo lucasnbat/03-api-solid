caa3929cd2fbd5bfb515c533dc838033b96dcf4a
Lucas Nunes Batista <lucasnunesb6@gmail.com>
2024-08-05 15:08:26 -0400

    adiciona caso de uso de métricas de usuário: numero de checkins

---
+++ src/use-cases/get-user-metrics.ts
@@ -0,0 +1,29 @@
+/**
+ * fetch: várias info;
+ * get: uma info só;
+ */
+
+import { CheckIn } from '@prisma/client'
+import { CheckInsRepository } from '@/repositories/check-ins-repository'
+
+interface GetUserMetricsUseCaseRequest {
+  userId: string
+}
+
+interface GetUserMetricsUseCaseResponse {
+  checkInsCount: number
+}
+
+export class GetUserMetricsUseCase {
+  constructor(private checkInsRepository: CheckInsRepository) {}
+
+  async execute({
+    userId,
+  }: GetUserMetricsUseCaseRequest): Promise<GetUserMetricsUseCaseResponse> {
+    const checkInsCount = await this.checkInsRepository.countByUserId(userId)
+
+    return {
+      checkInsCount,
+    }
+  }
+}
