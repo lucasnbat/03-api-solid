62e0275a6c95fc8abbdf2afb2e2ede7fd052c26b
Lucas Nunes Batista <lucasnunesb6@gmail.com>
2024-08-14 15:31:46 -0400

    adiciona teste do create gyms controller

--- src/http/controllers/gyms/create.spec.ts
+++ src/http/controllers/gyms/create.spec.ts
@@ -3,8 +3,9 @@ import { afterAll, beforeAll, describe, expect, it, test } from 'vitest'
 import request from 'supertest'
 import { app } from '@/app'
 import { createAndAuthenticateUser } from '@/utils/test/create-and-authenticate-user'
+import { title } from 'process'
 
-describe('Profile (e2e)', () => {
+describe('Create Gym (e2e)', () => {
   beforeAll(async () => {
     await app.ready() //evento emitido para aguradar app inicializar
   })
@@ -13,19 +14,20 @@ describe('Profile (e2e)', () => {
     await app.close()
   })
 
-  it('should be able to get usar profile', async () => {
+  it('should be able to create a gym', async () => {
     const { token } = await createAndAuthenticateUser(app)
 
-    const profileResponse = await request(app.server)
-      .get('/me')
+    const response = await request(app.server)
+      .post('/gyms')
       .set('Authorization', `Bearer ${token}`)
+      .send({
+          title: 'JS Gym',
+          description: 'Test',
+          phone: '66998897766',
+          latitude: -12.5566976,
+          longitude: -55.7187072,
+        })
 
-    expect(profileResponse.statusCode).toEqual(200)
-    expect(profileResponse.body.user).toEqual(
-      expect.objectContaining({
-        email: 'johndoe@example.com'
-      })
-    )
+      expect(response.statusCode).toEqual(201)
   })
-
 })
