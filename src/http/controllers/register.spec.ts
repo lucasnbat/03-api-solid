08fb094d813b50d0f4933ccb88fe5387b747c675
Lucas Nunes Batista <lucasnunesb6@gmail.com>
2024-08-14 10:56:00 -0400

    adiciona teste e2e para register

--- src/http/controllers/register.spec.ts
+++ src/http/controllers/register.spec.ts
@@ -1,5 +1,26 @@
 /* eslint-disable prettier/prettier */
-import { test } from 'vitest'
+import { afterAll, beforeAll, describe, expect, it, test } from 'vitest'
 import request from 'supertest'
+import { app } from '@/app'
 
-test('ok', () => { })
+describe('Register (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready() //evento emitido para aguradar app inicializar
+  })
+
+  afterAll(async () =>{
+    await app.close()
+  })
+
+  it('should be able to register', async () => {
+    const response = await request(app.server)
+      .post('/users')
+      .send({
+        name: 'John Doe',
+        email: 'johndoe@example.com',
+        password: '123123'
+      })
+      
+    expect(response.statusCode).toEqual(201)
+  })
+})
