d5e73be7e68b3b9c1c7c3851f4d1ff5bf86f2650
Lucas Nunes Batista <lucasnunesb6@gmail.com>
2024-08-15 14:38:07 -0400

    adiciona e configura rota de refresh token para user

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -3,10 +3,17 @@ import { register } from './register'
 import { authenticate } from './authenticate'
 import { profile } from './profile'
 import { verifyJWT } from '../../middlewares/verify-jwt'
+import { refresh } from './refresh'
 
 export async function userRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
+
+  /* 
+   * O refresh token vai ser justamente usado quando o 
+   * user não estiver mais autenticado 
+   */
+  app.patch('/token/refresh', refresh)
 
   /* If user is authenticated... */
   app.get('/me', { onRequest: [verifyJWT] }, profile)
